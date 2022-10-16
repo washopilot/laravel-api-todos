c2bc25da8ecb604ffc8c61e2eaefde1227d645b2
Fernando Chicaiza <washopilot@yahoo.com>
2022-10-16 11:03:11 -0500

    login template

--- resources/src/main.tsx
+++ resources/src/main.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './pages/App';
+import Login from './pages/Login';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider>
-            <App />
+            {/* <App /> */}
+            <Login />
         </ChakraProvider>
     </React.StrictMode>
 );
