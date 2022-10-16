73c784c29fb0953474147dd8848385ecf8c3bf90
Fernando Chicaiza <washopilot@yahoo.com>
2022-10-16 13:03:16 -0500

    install react-router

--- resources/src/main.tsx
+++ resources/src/main.tsx
@@ -2,15 +2,26 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './pages/App';
 import Login from './pages/Login';
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    }
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider>
-            {/* <App /> */}
-            <Login />
+            <RouterProvider router={router} />
         </ChakraProvider>
     </React.StrictMode>
 );
