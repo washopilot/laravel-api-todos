587e34fd0fd6444f37dd1a427bfeb05bc00173c1
Fernando Chicaiza <washopilot@yahoo.com>
2022-10-16 11:52:57 -0500

    some changes in Login

--- resources/src/pages/Login.tsx
+++ resources/src/pages/Login.tsx
@@ -1,41 +1,59 @@
+/* eslint-disable react/no-children-prop */
+import { AtSignIcon, LockIcon } from '@chakra-ui/icons';
 import {
+    Avatar,
     Box,
     Button,
-    Checkbox,
     Flex,
     FormControl,
-    FormLabel,
     Heading,
     Input,
+    InputGroup,
+    InputLeftElement,
+    InputRightElement,
     Link,
     Stack,
-    Text,
     useColorModeValue
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowClick = () => setShowPassword(!showPassword);
+
     return (
         <Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
             <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
+                    <Avatar bg="blue.400" />
                     <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-                    <Text fontSize={'lg'} color={'gray.600'}>
+                    {/* <Text fontSize={'lg'} color={'gray.600'}>
                         to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-                    </Text>
+                    </Text> */}
                 </Stack>
                 <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
                     <Stack spacing={4}>
                         <FormControl id="email">
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
+                            <InputGroup>
+                                <InputLeftElement pointerEvents="none" children={<AtSignIcon color="gray.300" />} />
+                                <Input type="email" placeholder="email address" />
+                            </InputGroup>
                         </FormControl>
                         <FormControl id="password">
-                            <FormLabel>Password</FormLabel>
-                            <Input type="password" />
+                            <InputGroup>
+                                <InputLeftElement pointerEvents="none" children={<LockIcon color="gray.300" />} />
+                                <Input type={showPassword ? 'text' : 'password'} placeholder="Password" />
+                                <InputRightElement width="4.5rem">
+                                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                                        {showPassword ? 'Hide' : 'Show'}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
                         </FormControl>
                         <Stack spacing={10}>
-                            <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
-                                <Checkbox>Remember me</Checkbox>
+                            <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'end'}>
+                                {/* <Checkbox>Remember me</Checkbox> */}
                                 <Link color={'blue.400'}>Forgot password?</Link>
                             </Stack>
                             <Button
